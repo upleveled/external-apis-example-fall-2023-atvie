1e13037ab3028f202b6fa12480112520d6f69e99
Karl Horky <karl.horky@gmail.com>
2023-10-12 13:39:31 +0200

    Add type

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,7 +1,11 @@
 import { NextResponse } from 'next/server';
 
+type StripeResponse = {
+  sessions: string;
+};
+
 // Export functions for each method to implement
-export function GET(): NextResponse<{ sessions: string }> {
+export function GET(): NextResponse<StripeResponse> {
   return NextResponse.json({
     sessions: '/api/sessions',
   });
